9f2a31a3502fdf98a833a2932696a709e622786c
Evgeniy <evgeniy@stereoflo.ru>
2018-05-10 18:20:11 +0300

    krasota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {AuthService} from './auth.service';
+import {AuthService} from './services/auth.service';
 
 
 @NgModule({
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {LoginInterface} from './interfeces/login-interface';
+import {LoginInterface} from '../interfeces/login-interface';
 
 @Injectable()
 export class AuthService {
