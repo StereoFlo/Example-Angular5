36b18495a4fb27a90412efefb31abe1dcbd25a5a
Evgeniy <evgeniy@stereoflo.ru>
2018-05-14 12:42:47 +0300

    code style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import {LoginComponent} from './login/login.component';
 
 import {FormsModule} from '@angular/forms';
 import {MainService} from './services/main.service';
-import { ErrorComponent } from './error/error.component';
-import { AdminComponent } from './admin/admin.component';
+import {ErrorComponent} from './error/error.component';
+import {AdminComponent} from './admin/admin.component';
 import {AdminService} from './services/admin.service';
 
 @NgModule({
