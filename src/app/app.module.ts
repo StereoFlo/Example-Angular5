a417ce8edeba496be4ff0f80863012e9d18c122b
Evgeniy <evgeniy@stereoflo.ru>
2018-05-14 12:45:04 +0300

    removed admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import {LoginComponent} from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import {MainService} from './services/main.service';
 import {ErrorComponent} from './error/error.component';
-import {AdminComponent} from './admin/admin.component';
 import {AdminService} from './services/admin.service';
 
 @NgModule({
@@ -21,7 +20,6 @@ import {AdminService} from './services/admin.service';
         PageComponent,
         LoginComponent,
         ErrorComponent,
-        AdminComponent
     ],
     imports: [
         BrowserModule,
